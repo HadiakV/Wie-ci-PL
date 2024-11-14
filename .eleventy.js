8cf23efe9f289f78be7e02995c3ced59b6104182
_HaDiaK_V_ <123464081+HadiakV@users.noreply.github.com>
2024-11-14 20:02:14 +0100

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,1 +1,13 @@
 
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("assets"); // Если есть изображения и стили
+
+  return {
+    dir: {
+      input: ".",       // Входной каталог
+      output: "_site",  // Папка для выходных файлов
+      includes: "_includes",
+      data: "_data"
+    }
+  };
+};

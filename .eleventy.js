0a7222e9dc1e1fb253bc3a1f728d9b1156525dda
_HaDiaK_V_ <123464081+HadiakV@users.noreply.github.com>
2024-11-16 17:23:55 +0100

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,8 @@
-
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("assets");
+    eleventyConfig.addCollection("posts", function (collectionApi) {
+        return collectionApi.getFilteredByGlob("posts/*.md").reverse();
+    });
+
     return {
         dir: {
             input: ".",
